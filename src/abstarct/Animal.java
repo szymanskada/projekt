e0adc90c4552d0670ce55f799181ef3cc6cca75c
Adrianna Szymańska <szymanskadrianna@gmail.com>
2023-06-22 15:32:30 +0200

    aktualizacja

--- src/abstarct/Animal.java
+++ src/abstarct/Animal.java
@@ -12,4 +12,5 @@ public abstract class Animal {
                 "name='" + name + '\'' +
                 '}';
     }
+
 }
--- src/abstarct/Mammal.java
+++ src/abstarct/Mammal.java
@@ -12,4 +12,5 @@ public abstract class Mammal extends Animal {
                 "name= " + name  +
                 "}}";
     }
+
 }
