b2cee1f6c09b34f55d946c114f6cb409977d789f
Adrianna Szymańska <szymanskadrianna@gmail.com>
2023-06-22 15:45:52 +0200

    aktualizacja test

---
+++ src/testMain/TestMain.java
@@ -0,0 +1,17 @@
+package testMain;
+
+import animals.Cat;
+import animals.Dog;
+
+public class TestMain {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Gordon");
+        System.out.println(cat);
+        cat.greets();
+
+        Dog dog =new Dog("Riko");
+        System.out.println(dog);
+        dog.greets();
+        dog.greets(dog);
+    }
+}
