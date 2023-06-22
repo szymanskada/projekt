ab0ccc8a1801b37dc55260c616216a9a42014a97
Adrianna Szymańska <szymanskadrianna@gmail.com>
2023-06-22 15:47:08 +0200

    aktualizacja animals

---
+++ src/animals/Dog.java
@@ -0,0 +1,21 @@
+package animals;
+
+import abstarct.Mammal;
+public class Dog extends Mammal {
+    public Dog(String name) {
+        super(name);
+    }
+    public void greets(){
+        System.out.println("Woof ");
+    }
+    public void greets(Dog another){
+        System.out.println("Wooof Wooooof ");
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{Mammal{Animal{" +
+                "name=" + name +
+                "}}}";
+    }
+}
